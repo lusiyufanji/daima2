47f77761464c2f0371cf3a8da3f8e9b7ff77d608
撕裂彡天堂丶 <1205251191@qq.com>
2017-11-27 18:35:36 +0800

    init

---
+++ src/components/Friends.vue
@@ -0,0 +1,142 @@
+<template>
+  <div>
+    <div class="friends-mianban clearfix">
+      <div class="friend-left fl">
+        <div class="friend-tab">
+          <span @click="friends = 1,groups =2,chatManyPeople = 2" :class="{ 'border-b':friends === 1,'border-none':friends === 2 }">&nbsp;好友&nbsp;</span>
+          <span @click="groups = 1,friends = 2,chatManyPeople = 2" :class="{ 'border-b':groups === 1,'border-none':groups === 2  }">&nbsp;群&nbsp;</span>
+          <span @click="chatManyPeople = 1,groups = 2,friends = 2" :class="{ 'border-b':chatManyPeople === 1,'border-none':chatManyPeople === 2  }">&nbsp;多人聊天&nbsp;</span>
+        </div>
+        <div class="friend-list" :class="{ 'visible':friends === 1,'hidden':friends === 2 }">
+          <ul>
+            <li>
+              <i class="fa fa-caret-down"></i>
+              <span>我的设备 <span class="f12">1/2</span></span>
+            </li>
+            <ul>
+              <li class="clearfix">
+                <div class="friends-box fl">
+                  <img src="../assets/phone-chat.png" alt="" class="fl">
+                  <div class="friends-test fl">
+                    <p>我的Android手机</p>
+                    <p class="color-7f">上次登录:2017-10-27 13</p>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </ul>
+        </div>
+        <div class="groups ":class="{ 'visible':groups === 1,'hidden':groups === 2 }">
+          123
+        </div>
+        <div class="chatManyPeople" :class="{ 'visible':chatManyPeople === 1,'hidden':chatManyPeople === 2 }">
+          123123
+        </div>
+      </div>
+      <div class="right-img fl">
+        <img src="../assets/bg-friends.png" alt="">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      friends:1,
+      groups:2,
+      chatManyPeople:2
+    }
+  }
+}
+</script>
+
+<style>
+  .friends-mianban{
+    width: 800px;
+    height: 540px;
+    overflow: hidden;
+    box-shadow: 0 3px 9px #898989;
+    position: relative;
+    top:-4px;
+  }
+  .friend-left{
+    width: 200px;
+    height: 100%;
+    background-color: #FAFAFA;
+    border-right: 1px solid #E8E9EA;
+  }
+  .friend-tab{
+    border-bottom: 1px solid #E8E9EA;
+    line-height: 40px;
+    font-size: 13px;
+    text-align: center;
+  }
+  .friend-tab span{
+    margin: 0 10px;
+    padding-bottom: 11px;
+    cursor: pointer;
+  }
+  .friend-tab span:hover{
+    border-bottom: 2px solid #4598FF;
+  }
+  .border-b{
+    border-bottom: 2px solid #4598FF;
+    color: #3A3A3A;
+  }
+  .border-none{
+    border-bottom: 2px solid transparent;
+    color: #767676;
+  }
+  .friend-list,.groups,.chatManyPeople{
+    visibility: hidden;
+    position: absolute;
+  }
+  .hidden{
+    visibility: hidden;
+  }
+  .visible{
+    visibility: visible;
+  }
+  .right-img{
+    width: 559px;
+    height: 100%;
+    text-align: center;
+  }
+  .right-img img{
+    margin-top: 150px;
+  }
+  .friend-list{
+    width: 200px;
+  }
+  .friend-list ul,.friend-list ul li{
+    width: 100%;
+  }
+  .friend-list ul li{
+    font-size: 14px;
+    line-height: 28px;
+  }
+  .friend-list ul li i{
+    margin-left: 10px;
+    margin-right: 5px;
+  }
+  .friend-list ul ul li:hover{
+    background-color: #EBEBEC;
+  }
+  .friend-list ul ul{
+    margin-top: 3px;
+  }
+  .friends-box{
+    padding: 3px 0;
+    cursor: pointer;
+  }
+  .friends-box img{
+    width: 42px;
+    margin: 6px 10px;
+  }
+  .color-7f{
+    color: #7F7F7F;
+    font-size: 12px;
+  }
+</style>
---
+++ src/components/mainMianBan.vue
@@ -0,0 +1,153 @@
+<template>
+  <div>
+    <div class="mianMianBan">
+      <div class="top-ban clearfix">
+        <div class="sm-touxiang fl"></div>
+        <div class="found">
+          <i class="fa fa-search"></i>
+        </div>
+        <div class="test-box clearfix">
+          <router-link to="/Message">
+            <div class="test-tab fl" @click="message = 1,friends = 2,onLineWord =2" :class="{ 'test-tab':message === 2,'active':message === 1 }">
+              <i class="test fa fa-wechat op"></i><br>
+              <span class="sanjiao op">&#9650</span>
+            </div>
+          </router-link>
+          <router-link to="/Friends">
+            <div class="test-tab fl" @click="message = 2,friends = 1,onLineWord =2" :class="{ 'test-tab':friends === 2,'active':friends === 1 }">
+              <i class="test fa fa-address-card-o op"></i><br>
+              <span class="sanjiao op">&#9650</span>
+            </div>
+          </router-link>
+          <router-link to="/onLineWord">
+            <div class="test-tab fl" @click="message = 2,friends = 2,onLineWord =1" :class="{ 'test-tab':onLineWord === 2,'active':onLineWord === 1 }" >
+              <i class="test fa fa-file-o op"></i><br>
+              <span class="sanjiao op">&#9650</span>
+            </div>
+          </router-link>
+        </div>
+        <div class="gongneng fl">
+          <i class="fa fa-navicon"></i>
+          <i class="fa fa-minus"></i>
+          <i class="fa fa-square-o"></i>
+          <i class="fa fa-times hover-red"></i>
+        </div>
+      </div>
+    <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import Firends from './Friends.vue'
+import Message from './Message.vue'
+import onLineWord from './onLineWord.vue'
+  export default {
+  data(){
+    return{
+      message:1,
+      friends:2,
+      onLineWord:2
+    }
+  },
+    components:{
+      Firends,
+      Message,
+      onLineWord,
+    }
+  }
+</script>
+
+<style scoped>
+  .mianMianBan{
+    margin-top: 5%;
+    margin-left: auto;
+    margin-right: auto;
+    width: 800px;
+  }
+  .top-ban{
+    height: 55px;
+    background-image: url("../assets/banenr-logo.png");
+    border-top-right-radius: 7px;
+    border-top-left-radius: 7px;
+    width: 800px;
+    overflow: hidden;
+    box-shadow: 0 -3px 9px #898989;
+  }
+  .sm-touxiang{
+    width: 40px;
+    height: 40px;
+    border-radius: 40px;
+    background-image: url("../assets/touxiang.jpg");
+    background-size: cover;
+    position: relative;
+    border: 1px solid #2E9BE9;
+    top: 7px;
+    left: 7px;
+  }
+  .sm-touxiang:hover{
+    border: 1px solid #D4EBFB ;
+  }
+  .found{
+    width: 95px;
+    height: 22px;
+    border-radius: 22px;
+    border: 1px solid #FFFFFF;
+    color: #FFFFFF;
+    opacity: 0.4;
+    position: relative;
+    left: 80px;
+    top: 15px;
+  }
+  .found i{
+    position: relative;
+    left: -32px;
+  }
+  .found:hover{
+    color: #FFFFFF;
+    opacity: 1.0;
+  }
+  .test-box{
+    margin-left: 195px;
+    margin-top: -10px;
+  }
+  .test-tab{
+    margin-right: 35px;
+  }
+  .test-tab .sanjiao{
+    position: relative;
+    color: #FFFFFF;
+    top:0;
+    left: 5px;
+    opacity: 0;
+  }
+  .test-tab i{
+    color: #FFFFFF;
+    font-size: 25px;
+    opacity: 0.7;
+  }
+  .test-box .active .op,.test-box .active .op{opacity: 1.0}
+  .test-box .test:hover{opacity: 1.0}
+
+
+  .gongneng{
+    position: relative;
+    left: 660px;
+    top: -8px;
+  }
+  .gongneng i{
+    color: #FFFFFF;
+    opacity: 0.6;
+    margin-right: 5px;
+  }
+  .gongneng i:hover{
+    opacity: 1.0;
+  }
+  .hover-red{
+    padding: 3px;
+  }
+  .hover-red:hover{
+    background-color: red;
+    padding: 3px;
+  }
+</style>
